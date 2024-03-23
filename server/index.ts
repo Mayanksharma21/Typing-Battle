c081c8b989f41874712bc2835e075c2c4dd47727
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2024-03-23 17:29:27 +0530

    Refactor Server for Production

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,17 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { setupListeners } from "./setupListeners";
+import { config } from "dotenv";
 
-const PORT = process.env.PORT || 8080;
+config();
+
+const PORT = process.env.PORT;
 
 const httpServer = createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: process.env.ALLOW_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
