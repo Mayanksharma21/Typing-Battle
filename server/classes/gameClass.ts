21d574eabea7025f1083edd37fc124629db98378
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2024-03-23 11:51:05 +0530

    setup Listeners for Server and Game Class join Player functionality

---
+++ server/classes/gameClass.ts
@@ -0,0 +1,48 @@
+import { Server, Socket } from "socket.io";
+
+export class GameClass {
+  gameStatus: "not-started" | "in-progress" | "finished";
+  gameId: string;
+  players: {
+    id: string;
+    score: number;
+    name: string;
+  }[];
+  io: Server;
+  gameHost: string;
+  paragraph: string;
+
+  constructor(roomId: string, io: Server, host: string) {
+    this.gameId = roomId;
+    this.players = [];
+    this.io = io;
+    this.gameHost = host;
+    this.gameStatus = "not-started";
+    this.paragraph = "";
+  }
+
+  setupListeners(socket: Socket) {}
+
+  joinPlayer(id: string, name: string, socket: Socket) {
+    if (this.gameStatus === "in-progress") {
+      return socket.emit(
+        "error",
+        "Game has already started, Pleasw wait for it to end before joining!!"
+      );
+    }
+
+    this.players.push({ id, name, score: 0 });
+
+    this.io.to(this.gameId).emit("player-joined", {
+      id,
+      name,
+      score: 0,
+    });
+
+    socket.emit("player", this.players);
+
+    socket.emit("new-host", this.gameHost);
+
+    this.setupListeners(socket);
+  }
+}
--- server/setupListeners.ts
+++ server/setupListeners.ts
@@ -1,7 +1,29 @@
 import { Server } from "socket.io";
+import { GameClass } from "./classes/gameClass";
+
+const rooms = new Map<string, GameClass>();
 
 export function setupListeners(io: Server) {
   io.on("connection", (socket) => {
     console.log(`New Connection with ID:- ${socket.id}`);
+
+    socket.on("join-game", (roomId: string, name: string) => {
+      if (!roomId) return socket.emit("error", "Invalid Room Id");
+      if (!name) return socket.emit("error", "Please provide a nickname");
+
+      socket.join(roomId);
+
+      if (rooms.has(roomId)) {
+        const game = rooms.get(roomId);
+        if (!game) return socket.emit("error", "Game not found");
+
+        game.joinPlayer(socket.id, name, socket);
+      } else {
+        const game = new GameClass(roomId, io, socket.id);
+        rooms.set(roomId, game);
+
+        game.joinPlayer(socket.id, name, socket);
+      }
+    });
   });
 }
